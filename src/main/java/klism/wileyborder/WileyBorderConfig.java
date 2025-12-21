586eb5c4745c70651462cc0694094cbff535e253
Josh <josh.seungwon.hong@gmail.com>
2025-12-20 23:21:53 -0500

    Add globals

--- src/main/java/klism/wileyborder/WileyBorderConfig.java
+++ src/main/java/klism/wileyborder/WileyBorderConfig.java
@@ -1,4 +1,12 @@
 package klism.wileyborder;
 
-public class WileyBorderConfig {
+public final class WileyBorderConfig {
+    public Axis axis = Axis.X;
+    public int coordinate = 0;
+
+    //Border color
+    public int argb = 0x33FF0000;
+
+    //Show overlay?
+    public boolean enabled = true;
 }
