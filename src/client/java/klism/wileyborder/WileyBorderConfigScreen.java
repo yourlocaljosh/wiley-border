4f49cec54b098c7813648191116ffb341adbee10
Josh <josh.seungwon.hong@gmail.com>
2025-12-20 23:33:36 -0500

    Create configscreen file and add temporary stub until cloth config dependency implemented

---
+++ src/client/java/klism/wileyborder/WileyBorderConfigScreen.java
@@ -0,0 +1,12 @@
+package klism.wileyborder;
+
+import net.minecraft.client.gui.screen.Screen;
+
+public final class WileyBorderConfigScreen {
+    private WileyBorderConfigScreen() {}
+
+    public static Screen create(Screen parent) {
+        //temp stub
+        return parent;
+    }
+}
