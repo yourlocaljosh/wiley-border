e943daada89e88130bc43c900bf0ace1240ce959
Josh <josh.seungwon.hong@gmail.com>
2025-12-21 00:12:51 -0500

    Add four fields for individual border

---
+++ src/main/java/klism/wileyborder/BorderConfig.java
@@ -0,0 +1,8 @@
+package klism.wileyborder;
+
+public final class BorderConfig {
+    public boolean enabled = false;
+    public Axis axis = Axis.X;
+    public int coordinate = 0;
+    public int argb = 0x33FF0000;
+}
