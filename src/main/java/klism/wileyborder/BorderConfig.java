9bfaaf4592453cf910e80757ba110ec1ce1a74c6
Josh <josh.seungwon.hong@gmail.com>
2025-12-21 00:29:24 -0500

    Change hex to 6 digit to remove alpha option with opacity option instead

--- src/main/java/klism/wileyborder/BorderConfig.java
+++ src/main/java/klism/wileyborder/BorderConfig.java
@@ -4,5 +4,7 @@ public final class BorderConfig {
     public boolean enabled = false;
     public Axis axis = Axis.X;
     public int coordinate = 0;
-    public int argb = 0x33FF0000;
+    //switch to hex
+    public int rgb = 0xFF0000;
+    public float opacity = 0.20f;
 }
