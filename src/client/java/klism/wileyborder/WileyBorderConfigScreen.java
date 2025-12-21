ad2b098943031c616dd9f5ea3081f4d0eb004ebe
Josh <josh.seungwon.hong@gmail.com>
2025-12-21 00:02:07 -0500

    Update w cloth config dependencies and rebuild

--- src/client/java/klism/wileyborder/WileyBorderConfigScreen.java
+++ src/client/java/klism/wileyborder/WileyBorderConfigScreen.java
@@ -1,12 +1,56 @@
 package klism.wileyborder;
 
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
 
 public final class WileyBorderConfigScreen {
     private WileyBorderConfigScreen() {}
 
     public static Screen create(Screen parent) {
-        //temp stub
-        return parent;
+        WileyBorderConfig cfg = ConfigManager.get();
+
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.literal("Wiley Border"))
+                .setSavingRunnable(ConfigManager::save);
+
+        ConfigCategory general = builder.getOrCreateCategory(Text.literal("General"));
+        ConfigEntryBuilder eb = builder.entryBuilder();
+
+        general.addEntry(
+                eb.startBooleanToggle(Text.literal("Enabled"), cfg.enabled)
+                        .setDefaultValue(true)
+                        .setSaveConsumer(val -> cfg.enabled = val)
+                        .build()
+        );
+
+        general.addEntry(
+                eb.startEnumSelector(Text.literal("Axis"), Axis.class, cfg.axis)
+                        .setDefaultValue(Axis.X)
+                        .setSaveConsumer(val -> cfg.axis = val)
+                        .build()
+        );
+
+        general.addEntry(
+                eb.startIntField(Text.literal("Coordinate"), cfg.coordinate)
+                        .setDefaultValue(0)
+                        .setMin(Integer.MIN_VALUE)
+                        .setMax(Integer.MAX_VALUE)
+                        .setSaveConsumer(val -> cfg.coordinate = val)
+                        .build()
+        );
+
+        general.addEntry(
+                eb.startColorField(Text.literal("Plane Color"), cfg.argb)
+                        .setDefaultValue(0x33FF0000)
+                        .setAlphaMode(true)
+                        .setSaveConsumer(val -> cfg.argb = val)
+                        .build()
+        );
+
+        return builder.build();
     }
 }
