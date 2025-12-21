0973cb35618648de4fbca7e4d8ab5b683bb297df
Josh <josh.seungwon.hong@gmail.com>
2025-12-20 23:24:34 -0500

    Initialize WileyBorderClient

--- src/client/java/klism/wileyborder/WileyBorderClient.java
+++ src/client/java/klism/wileyborder/WileyBorderClient.java
@@ -2,9 +2,10 @@ package klism.wileyborder;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class WileyBorderClient implements ClientModInitializer {
+public final class WileyBorderClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ConfigManager.load();
+		PlaneRenderer.register();
 	}
 }
